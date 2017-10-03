fbc103908d89237c65d76d4a50bfc74ff7f25f02
ShediShynne <mashedishynne_catagasan@umindanao.edu.ph>
2017-10-03 18:30:41 +0800

    Shedi Added

---
+++ ShediHelloWorldGIT/ShediHelloWorld/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_dontpush = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(54, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // btn_dontpush
+            // 
+            this.btn_dontpush.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_dontpush.BackColor = System.Drawing.Color.DarkRed;
+            this.btn_dontpush.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dontpush.ForeColor = System.Drawing.Color.White;
+            this.btn_dontpush.Location = new System.Drawing.Point(43, 48);
+            this.btn_dontpush.Name = "btn_dontpush";
+            this.btn_dontpush.Size = new System.Drawing.Size(191, 69);
+            this.btn_dontpush.TabIndex = 1;
+            this.btn_dontpush.Text = "Don\'t push the button";
+            this.btn_dontpush.UseVisualStyleBackColor = false;
+            this.btn_dontpush.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 179);
+            this.Controls.Add(this.btn_dontpush);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_dontpush;
+    }
+}
+
